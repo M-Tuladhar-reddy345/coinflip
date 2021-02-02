5573a20a1121805e27b5dc9f4a50554091f90c56
Tuladhar <tuladharreddy345@gmail.com>
2021-02-02 09:13:26 +0530

    initial commit

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 function flippppp(){
-  l = Array(1,0,1,0,1,0,1,1,0,1)
+  l = Array(1,1,0,0,1,1,0,1,0,0)
   x = l[Math.floor(Math.random()*l.length + 1)];
   console.log(x);
   if (x == 0){
     var text = document.createElement("H1");
     text.innerHTML = 'heads win';
+    text.classname = 'texty';
     document.body.appendChild(text);
   }
   else if (x == 1) {
     var text = document.createElement("H1");
+    text.class = 'texty'
     text.innerHTML = 'tails win';
     document.body.appendChild(text);
   }
